6bf135f5e90994c3ed9329cd4ad997f5f58290c2
Mark <h03514@gmail.com>
2022-08-18 21:44:33 +0800

    增加Regex.Match 方法

--- NcccToTxt/Program.cs
+++ NcccToTxt/Program.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Threading.Tasks;
 using System.Data;
 using System.Globalization;
+using System.Text.RegularExpressions;
 
 namespace NcccToTxt
 {
@@ -19,9 +20,17 @@ namespace NcccToTxt
             //{
             try
             {
-                foreach (var line in di.GetFiles("*.txt"))
+
+
+                string pattern = @"\b\w*I87+\w*\b";
+
+
+                foreach (var line in di.GetFiles("*"))
                 {
-                    if (line.Name == "test(1).txt")
+                    Match m = Regex.Match(line.Name, pattern);
+
+                    if(m.Success)
+                    //if (line.Name == "test(1).txt")
                     {
                         path = @"D:\\桌面\\新增資料夾 (4)\\ddd\\1.txt";
 
@@ -31,34 +40,8 @@ namespace NcccToTxt
                            
                             List<string> lists = new List<string>();
 
-                            // Open the stream and read it back.
-                            using (StreamReader sr = File.OpenText(@"D:\桌面\新增資料夾 (4)\" + line.Name))
-                            {
-                                DataTable dt = new DataTable();
-                                string s = "";
-                                while ((s = sr.ReadLine()) != null)
-                                {
-                                    string aa = s.Substring(0, 3);
-                                    string bb = s.Substring(3, 7);
-
-                                    if (aa == "0CF")
-                                    {
-                                        //dt.Rows.Add(new object[] { "1".PadRight(5) + aa.PadRight(10) + bb });
-                                    }
-
-                                    if (aa == "0CH")
-                                    {
-                                        //dt.Rows.Add(new object[] { "2".PadRight(5) + aa.PadRight(10) + bb });
-                                    }
-
-                                    if (aa == "0CE")
-                                    {
-                                        //dt.Rows.Add(new object[] { "3".PadRight(5) + aa.PadRight(10) + bb });
-                                    }
-
-
-                                }
-                            }
+                           
+                            fs.Close();
 
                             //System.Globalization.TaiwanCalendar tc = new System.Globalization.TaiwanCalendar();
 
@@ -80,10 +63,60 @@ namespace NcccToTxt
                             //fs.Write(info, 0, info.Length);
                             //Console.WriteLine("ad");
                         }
+                        // Open the stream and read it back.
+                        using (StreamReader sr = new StreamReader(@"D:\桌面\新增資料夾 (4)\" + line.Name))
+                        {
+                            DataTable dt = new DataTable();
+                            string s = "";
+                            while ((s = sr.ReadLine()) != null)
+                            {
+                                string aa = s.Substring(0, 3);
+                                string bb = s.Substring(3, 7);
+
+                                if (aa == "0CF")
+                                {
+                                    //dt.Rows.Add(new object[] { "1".PadRight(5) + aa.PadRight(10) + bb });
+                                }
+
+                                if (aa == "0CH")
+                                {
+                                    //dt.Rows.Add(new object[] { "2".PadRight(5) + aa.PadRight(10) + bb });
+                                }
+
+                                if (aa == "0CE")
+                                {
+                                    //dt.Rows.Add(new object[] { "3".PadRight(5) + aa.PadRight(10) + bb });
+                                }
+
+
+                            }
+                            
+                            //swr.WriteLine(srr.ReadToEnd());
+                            sr.Close();
+
+                        }
+
+                        //using (StreamReader srr = new StreamReader(@"D:\桌面\新增資料夾 (4)\ddd\" + "1.txt"))
+                        //{
+                        //    StreamWriter swr = new StreamWriter(@"D:\桌面\新增資料夾 (4)\ddd\" + "1.txt", false, Encoding.Default);
+
+                        //    // invoke the ReadToEnd method
+                        //    swr.WriteLine(srr.ReadToEnd());
+
+                        //    swr.Close();
+                        //    srr.Close();
+                        //}
+
+
+
                         Console.WriteLine("D:\\桌面\\新增資料夾 (4)\\" + line.ToString());
                         //File.Move();
                         FileMove("D:\\桌面\\新增資料夾 (4)\\" + line.ToString(), "D:\\桌面\\新增資料夾 (4)\\done\\" + line.ToString());
+                        new StreamWriter((@"D:\桌面\新增資料夾 (4)\ddd\" + "1.txt").Replace(".txt", "-ansi.txt"), false, Encoding.ASCII);
+
                     }
+
+                   
 
 
                     if (line.Name == "test(2).txt")
@@ -94,6 +127,8 @@ namespace NcccToTxt
 
 
                
+
+
             }
 
             catch (Exception ex)
